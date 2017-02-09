e8e57261949ceba38bf6b4e9d24056e1b8ee95ca
aliciasavelly <alicia.savelly@gmail.com>
2017-02-08 23:20:03 -0800

    finish queries, passing all specs

--- movie_novice/queries.rb
+++ movie_novice/queries.rb
@@ -56,27 +56,6 @@ def below_average_years
   .order("bad_movies DESC")
 end
 
-# == Schema Information
-#
-# Table name: actors
-#
-#  id          :integer      not null, primary key
-#  name        :string
-#
-# Table name: movies
-#
-#  id          :integer      not null, primary key
-#  title       :string
-#  yr          :integer
-#  score       :float
-#  votes       :integer
-#  director_id :integer
-#
-# Table name: castings
-#  id          :integer      not null, primary key
-#  movie_id    :integer      not null
-#  actor_id    :integer      not null
-#  ord         :integer
 
 def alphabetized_actors
   # display the first 10 actor names ordered from A-Z
@@ -101,10 +80,37 @@ def pulp_fiction_actors
     .where("movies.title = 'Pulp Fiction'").uniq
 end
 
+# == Schema Information
+#
+# Table name: actors
+#
+#  id          :integer      not null, primary key
+#  name        :string
+#
+# Table name: movies
+#
+#  id          :integer      not null, primary key
+#  title       :string
+#  yr          :integer
+#  score       :float
+#  votes       :integer
+#  director_id :integer
+#
+# Table name: castings
+#  id          :integer      not null, primary key
+#  movie_id    :integer      not null
+#  actor_id    :integer      not null
+#  ord         :integer
+
 def uma_movies
   #practice using joins
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-
+  Movie
+    .select(:id, :title, :yr)
+    .joins(:castings)
+    .joins(:actors)
+    .where("name = 'Uma Thurman'")
+    .order("yr ASC").uniq
 end
