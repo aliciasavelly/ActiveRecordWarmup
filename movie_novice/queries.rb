565791b8d1c61d68b058355f3d4716620ef1ffe6
aliciasavelly <alicia.savelly@gmail.com>
2017-02-08 23:12:16 -0800

    add more to movie_novice\queries file. passing 5/7 specs

--- movie_novice/queries.rb
+++ movie_novice/queries.rb
@@ -23,19 +23,24 @@
 
 def find_angelina
   #find Angelina Jolie by name in the actors table
-  Actor.find_by(name: 'Angelina Jolie')
+  Actor
+    .find_by(name: 'Angelina Jolie')
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-  Movie.select(:id, :title).where("score >= 9")
+  Movie
+    .select(:id, :title)
+    .where("score >= 9")
 end
 
 def star_wars
   #display the id, title and year of each Star Wars movie in movies.
   # hint: use 'select' and 'where'
-
+  Movie
+    .select(:id, :title, :yr)
+    .where("title LIKE '%Star Wars%'")
 end
 
 
@@ -44,8 +49,34 @@ def below_average_years
   #with the count of movies scoring under 5 aliased as bad_movies,
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
-
+  Movie
+  .select(:yr, "COUNT(*) AS bad_movies")
+  .where("score < 5")
+  .group("yr")
+  .order("bad_movies DESC")
 end
+
+# == Schema Information
+#
+# Table name: actors
+#
+#  id          :integer      not null, primary key
+#  name        :string
+#
+# Table name: movies
+#
+#  id          :integer      not null, primary key
+#  title       :string
+#  yr          :integer
+#  score       :float
+#  votes       :integer
+#  director_id :integer
+#
+# Table name: castings
+#  id          :integer      not null, primary key
+#  movie_id    :integer      not null
+#  actor_id    :integer      not null
+#  ord         :integer
 
 def alphabetized_actors
   # display the first 10 actor names ordered from A-Z
@@ -53,7 +84,10 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
-
+  Actor
+    .select(:id, :name)
+    .order('name')
+    .limit('10')
 end
 
 def pulp_fiction_actors
